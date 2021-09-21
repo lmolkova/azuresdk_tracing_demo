77aeabf39bb1615b12b03cd317e425f8e932bb7b
Liudmila Molkova <limolkova@microsoft.com>
2021-09-21 15:34:13 -0700

    up

--- python/manage.py
+++ python/manage.py
@@ -15,7 +15,6 @@ from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from azure.monitor.opentelemetry.exporter import AzureMonitorTraceExporter
 from opentelemetry.instrumentation.django import DjangoInstrumentor
-from opentelemetry.instrumentation.urllib3 import URLLib3Instrumentor
 from opentelemetry.instrumentation.requests import RequestsInstrumentor
 
 from azure.eventhub import EventHubConsumerClient
@@ -27,12 +26,14 @@ eventhub_name = os.environ['EVENTHUB_NAME']
 storage_connection_str = os.environ['CHECKPOINT_STORAGE_CONNECTION_STRING']
 container_name = os.environ['CHECKPOINT_CONTAINER_NAME']
 
-async  def on_event(partition_context, event):
+def on_event(partition_context, event):
+    print("event!!!!!!!!")
     print(event)
-    await partition_context.update_checkpoint(event)  # Or update_checkpoint every N events for better performance.
+    partition_context.update_checkpoint(event)  # Or update_checkpoint every N events for better performance.
 
 
-async def receive_messages():
+def receive_messages():
+    print("receiving messages")
     checkpoint_store = BlobCheckpointStore.from_connection_string(
         storage_connection_str,
         container_name
@@ -43,10 +44,10 @@ async def receive_messages():
         eventhub_name=eventhub_name,
         checkpoint_store=checkpoint_store,
     )
-    async with client:
-        await client.receive(on_event)
+    with client:
+        client.receive(on_event)
 
-async def main():
+def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tracingdemo.settings')
     try:
@@ -60,7 +61,6 @@ async def main():
 
     DjangoInstrumentor().instrument()
     RequestsInstrumentor().instrument()
-    URLLib3Instrumentor().instrument()
 
     trace.set_tracer_provider(TracerProvider())
     tracer = trace.get_tracer(__name__)
@@ -69,10 +69,7 @@ async def main():
     span_processor = BatchSpanProcessor(exporter)
     trace.get_tracer_provider().add_span_processor(span_processor)
     execute_from_command_line(sys.argv)
-    await receive_messages()
-
-
+    receive_messages()
 
 if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+    main()
